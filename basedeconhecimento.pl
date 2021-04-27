46399993f7ac3964b5a1af222417a5366e3093fe
britohelena <53785016+britohelena@users.noreply.github.com>
2021-04-27 18:53:04 +0100

    Add files via upload

---
+++ basedeconhecimento.pl
@@ -0,0 +1,18 @@
+% perfis %
+
+% carnivora %
+
+
+% vegetariana %
+
+
+% vegan (helena) %
+if 'vegan' and 'entrada' and 'gluten' then 'vegan1'.
+if 'vegan' and 'entrada' and 'lactose' then 'vegan2'.
+if 'vegan' and 'entrada' and 'nenhuma' then 'vegan3'.
+if 'vegan' and 'principal' and 'gluten' then 'vegan4'.
+if 'vegan' and 'principal' and 'lactose' then 'vegan5'.
+if 'vegan' and 'principal' and 'nenhuma' then 'vegan6'.
+if 'vegan' and 'sobremesa' and 'gluten' then 'vegan7'.
+if 'vegan' and 'sobremesa' and 'lactose' then 'vegan8'.
+if 'vegan' and 'sobremesa' and 'nenhuma' then 'vegan9'.
