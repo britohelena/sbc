d5bae016d35042300d23d5de311c12328638db13
britohelena <53785016+britohelena@users.noreply.github.com>
2021-04-27 22:01:29 +0100

    Add files via upload

--- basedados.pl
+++ basedados.pl
@@ -16,7 +16,7 @@ solucao('vegan1',['\n Poke Bowl com Jaca
                    \n Pode ser utilizado como entrada, servido com legumes crus, acompanhamento de saladas entre outros.
                    \n Típico do médio oriente.']).
 
-solucao('vegan2',['\n Queijo" fresco vegano de caju
+solucao('vegan2',['\n Queijo" fresco vegan de caju
                    \n Perfeito para comer com tostas ou com pão.']
                  ['\n Hummus de abóbora
                    \n Versátil e delicioso.']).
@@ -26,34 +26,34 @@ solucao('vegan3',['\n Pan con Tomate
                  ['\n Rissóis de Cogumelos e Chouriço de Seitan
                    \n Saboroso e leve.']). 
 
-solucao('vegan4',['\n 
-                   \n ']
-                 ['\n 
-                   \n ']).    
+solucao('vegan4',['\n Almôndegas vegan
+                   \n Feitas com feijões azuqui.']
+                 ['\n Tabule de Quinoa
+                   \n Prato saudável e prático.']).    
 
-solucao('vegan5',['\n 
-                   \n ']
-                 ['\n 
-                   \n ']). 
+solucao('vegan5',['\n Molho branco vegan sem lactose
+                   \n Poucas calorias e saudável.']
+                 ['\n Torta de brócolos
+                   \n Super nutritiva.']). 
 
-solucao('vegan6',['\n
-                   \n ']
-                 ['\n 
-                   \n ']). 
+solucao('vegan6',['\n Quiche de requeijão com cebolas caramelizadas
+                   \n Recheio cremoso com uma casca fina e crocante e com o doce das cebolas.']
+                 ['\n Sopa de legumes
+                   \n Clássica e deliciosa.']). 
 
-solucao('vegan7',['\n 
-                   \n ']
-                 ['\n 
-                   \n ']). 
+solucao('vegan7',['\n Cookies de chocolate sem glúten
+                   \n Com ingredientes muito nutritivos.']
+                 ['\n Bolo de maçã vegan sem glúten
+                   \n Bolo simples que perfuma a casa inteira.']). 
 
-solucao('vegan8',['\n 
-                   \n ']
-                 ['\n 
-                   \n ']). 
+solucao('vegan8',['\n Gelados de côco sem lactose e açúcar
+                   \n Fresco e ideal para tempos quentes.']
+                 ['\n Pão de banana sem ovos e lactose
+                   \n Super simples mas delicioso.']). 
 
-solucao('vegan9',['\n 
-                   \n ']
-                 ['\n 
-                   \n ']). 
+solucao('vegan9',['\n Cookies vegan de coco e canela
+                   \n Perfeito para quando se tem a vontade de algo doce.']
+                 ['\n Petit Gateau vegan
+                   \n Delicioso e vai impressionar toda a gente!']). 
 
 
