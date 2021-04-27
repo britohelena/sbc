10cd1da2ea2c4b713638b8f48678c46a56c4e798
britohelena <53785016+britohelena@users.noreply.github.com>
2021-04-27 20:15:13 +0100

    Add files via upload

--- basedados.pl
+++ basedados.pl
@@ -1,4 +1,5 @@
 :-dynamic(fact/1),[forward,proof].
+solucao()
 
 % carnivora %
 
@@ -6,14 +7,53 @@
 % vegetariana %
 
 
-% vegan (helena)%
+% vegan (helena) %
+
 solucao('vegan1',['\n Poke Bowl com Jaca
                    \n Comida rápida e cheia de sabor que conforta o estômago.
-                   \n Vegan e usa trigo sarraceno por isso não se tem de preocupar com o glúten.
-                   \n Ótimo para o ínicio da refeição.']
+                   \n Vegan e usa trigo sarraceno por isso não se tem de preocupar com o glúten.']
                  ['\n Hummus Vermelho (pimento fumado)
                    \n Pode ser utilizado como entrada, servido com legumes crus, acompanhamento de saladas entre outros.
-                   \n Típico do médio oriente.
-                   \n Ótimo para o ínicio da refeição.']).
+                   \n Típico do médio oriente.']).
+
+solucao('vegan2',['\n Queijo" fresco vegano de caju
+                   \n Perfeito para comer com tostas ou com pão.']
+                 ['\n Hummus de abóbora
+                   \n Versátil e delicioso.']).
+
+solucao('vegan3',['\n Pan con Tomate
+                   \n Simples e delicioso de Barcelona!']
+                 ['\n Rissóis de Cogumelos e Chouriço de Seitan
+                   \n Saboroso e leve.']). 
+
+solucao('vegan4',['\n 
+                   \n ']
+                 ['\n 
+                   \n ']).    
+
+solucao('vegan5',['\n 
+                   \n ']
+                 ['\n 
+                   \n ']). 
+
+solucao('vegan6',['\n
+                   \n ']
+                 ['\n 
+                   \n ']). 
+
+solucao('vegan7',['\n 
+                   \n ']
+                 ['\n 
+                   \n ']). 
+
+solucao('vegan8',['\n 
+                   \n ']
+                 ['\n 
+                   \n ']). 
+
+solucao('vegan9',['\n 
+                   \n ']
+                 ['\n 
+                   \n ']). 
 
 
