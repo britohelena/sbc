a408c61effb614319103938a673c77c664a6c3bd
Tomas-Lopes <56835128+Tomas-Lopes@users.noreply.github.com>
2021-05-17 21:56:40 +0100

    .

---
+++ ParteA/basedados.pl
@@ -0,0 +1,155 @@
+:-dynamic(fact/1),[forward,proof].
+solucao()
+
+% carnivora %
+solucao('carnivora1',['\n Bruschetta de tomates
+                   \n A clássica bruschetta de tomates tem um gostinho especial da Itália e é fácil de fazer.
+                   \n Perfeita para abrir o apetite antes do jantar.']
+                 ['\n Sopa de cebola
+                   \n Prato clássico francês.
+                   \n Perfeita para ser servida no inverno.']).
+
+solucao('carnivora2',['\n Tartare de salmão cozido
+                   \n Prato originário dos Tártaros, grupo étnico que habita a Rússia e países vizinhos.
+                   \n A mistura do toque picante e adocicado da geleia de pimenta com o azedinho do cottage se afinam super bem com o salmão.']
+                 ['\n Palitos de beringela
+                   \n Super delicioso para acompanhar uma refeição, para substituir as famosas batatas fritas do cheeseburguer ou ainda como uma entrada servida com uma massa ou molho cremoso']).
+
+solucao('carnivora3',['\n Sopa fria de tomate
+                   \n É entrada para qualquer celebração, faça sol ou faça chuva, já que fica bem com qualquer prato que venha a seguir']
+                 ['\n Gaspacho à alentejana
+                   \n O gaspacho à alentejana é um dos pratos típicos da região. Um prato fresco para servir nos dias quentes, uma ideia excelente para começar um almoço de amigos']). 
+
+solucao('carnivora4',['\n Cannelloni Gratinado de Presunto Parma, Pesto e Mussarela de Búfala
+                   \n Esta deliciosa receita de Cannelloni sem glúten preparada com a massa de lasanha seca é absolutamente deliciosa.']
+                 ['\n Lasanha de Bacalhau com Molho Branco
+                   \n O prato perfeito para reunir a família ao redor da mesa: lasanha de bacalhau sem glúten com molho branco!']).
+
+solucao('carnivora5',['\n  Prego no Prato
+                   \n Tradicional da cozinha portuguesa, cuja receita é muito prática e simples']
+                 ['\n Cozido à portuguesa
+                   \n Um cozido tradicional para amantes de carne, este prato inclui frango, carne de vaca, porco e várias salsichas de porco.']).
+
+solucao('carnivora6',['\n Francesinha
+                   \n Típica da cidade do Porto é uma versão rebelde do Croque Monsieur francês, o que parece justificar o seu nome fantástico sabor.']
+                 ['\n Caldeirada de peixe
+                   \n Contém vários tipos de peixe e marisco, coberto de tomates e ervas aromáticas. O jantar ideal após um longo dia de praia.']).
+
+solucao('carnivora7',['\n Bolo de ananás com nozes
+                   \n Receita fácil e prática.
+                   \n Perfeita para servir ao fim de um almoço ou jantar.']
+                 ['\n Bolo de iogurte sem glúten
+                   \n Super delicioso']).
+
+solucao('carnivora8',['\n Pudim sem lactose
+                   \n Receita deliciosamente cremosa.']
+                 ['\n Brigadeiros sem lactose
+                   \n Fácil de se fazer']).
+
+solucao('carnivora9',['\n Bolo de bolacha
+                   \n Clássico das sobremesas.
+                   \n Perfeita para o final de um almoço de domingo.']
+                 ['\n Cheesecake de morango
+                   \n Receita deliciosa que nenhum convidado recusa']).
+                   
+% vegetariana %
+
+solucao('vegetariana1',['\n Pataniscas de Legumes
+                   \n Muito fácil de fazer e leva apenas 6 ingredientes.
+                   \n Perfeita para abrir o apetite antes de um almoço ou jantar.']
+                 ['\n Hummus de Abóbora
+                   \n Receita rápida, fácil e deliciosa.
+                   \n Perfeito para ser servido no Natal.']).
+                   
+solucao('vegetariana2',['\n Queijo vegetariano de amêndoa
+                   \n Boa para servir em tempos Natalícios.
+                   \n ótimo para barrar.']
+                 ['\n Hummus de feijão branco picante
+                   \n ótima entrada para qualquer refeição
+                   \n Perfeito para quem é alérgico à lactose.']).
+
+solucao('vegetariana3',['\n Pasta de sementes de Girassol
+                   \n Saudável, fácil de fazer e súper deliciosa.
+                   \n Perfeita para servir com pães, frios e queijos.']
+                 ['\n Chamuças Vegetarianas
+                   \n Prato tipicamente indiano.
+                   \n Perfeito para ser servido aos seus convidados.']).
+
+solucao('vegetariana4',['\n Falafel de forno 
+                   \n Bolinhos de grão de bico deliciosos.']
+                 ['\n Lasanha de courgette vegetariana sem gluten
+                   \n Prato rico em vitaminas e minerais.']).
+
+solucao('vegetariana5',['\n Abóbora recheada com legumes e quinoa
+                   \n Clássica e deliciosa.']
+                 ['\n Caril de lentilhas
+                   \n Prato delicios e nutritivo.']). 
+
+solucao('vegetariana6',['\n Massa com vegetais grelhados
+                   \n Refeição leve e nutritiva.']
+                 ['\n Tortellini de espinafres
+                   \n Uma deliciosa refeição para toda a família.']). 
+
+solucao('vegetariana7',['\n Gelatina de frutos silvestres com hortelã
+                   \n Sobremesa ideal para o verão.']
+                 ['\n Gelado de kiwi e melancia com mel
+                   \n Melhor forma de se refrescar num dia quente de verão.']). 
+
+solucao('vegetariana8',['\n Crumble de maçã
+                   \n Sobremesa muito saborosa.']
+                 ['\n Pudim de chia
+                   \n Acompanhado com fruta fresca.']). 
+
+solucao('vegetariana9',['\n Mousse de chocolate vegetariana
+                   \n Saudável e com todo o sabor da mousse tradicional.']
+                 ['\n Tarte de abóbora
+                   \n Deliciosa e com sabores natalícios.']).
+
+% vegan (helena) %
+
+solucao('vegan1',['\n Poke Bowl com Jaca
+                   \n Comida rápida e cheia de sabor que conforta o estômago.
+                   \n Vegan e usa trigo sarraceno por isso não se tem de preocupar com o glúten.']
+                 ['\n Hummus Vermelho (pimento fumado)
+                   \n Pode ser utilizado como entrada, servido com legumes crus, acompanhamento de saladas entre outros.
+                   \n Típico do médio oriente.']).
+
+solucao('vegan2',['\n Queijo" fresco vegan de caju
+                   \n Perfeito para comer com tostas ou com pão.']
+                 ['\n Hummus de abóbora
+                   \n Versátil e delicioso.']).
+
+solucao('vegan3',['\n Pan con Tomate
+                   \n Simples e delicioso de Barcelona!']
+                 ['\n Rissóis de Cogumelos e Chouriço de Seitan
+                   \n Saboroso e leve.']). 
+
+solucao('vegan4',['\n Almôndegas vegan
+                   \n Feitas com feijões azuqui.']
+                 ['\n Tabule de Quinoa
+                   \n Prato saudável e prático.']).    
+
+solucao('vegan5',['\n Molho branco vegan sem lactose
+                   \n Poucas calorias e saudável.']
+                 ['\n Torta de brócolos
+                   \n Super nutritiva.']). 
+
+solucao('vegan6',['\n Quiche de requeijão com cebolas caramelizadas
+                   \n Recheio cremoso com uma casca fina e crocante e com o doce das cebolas.']
+                 ['\n Sopa de legumes
+                   \n Clássica e deliciosa.']). 
+
+solucao('vegan7',['\n Cookies de chocolate sem glúten
+                   \n Com ingredientes muito nutritivos.']
+                 ['\n Bolo de maçã vegan sem glúten
+                   \n Bolo simples que perfuma a casa inteira.']). 
+
+solucao('vegan8',['\n Gelados de côco sem lactose e açúcar
+                   \n Fresco e ideal para tempos quentes.']
+                 ['\n Pão de banana sem ovos e lactose
+                   \n Super simples mas delicioso.']). 
+
+solucao('vegan9',['\n Cookies vegan de coco e canela
+                   \n Perfeito para quando se tem a vontade de algo doce.']
+                 ['\n Petit Gateau vegan
+                   \n Delicioso e vai impressionar toda a gente!']). 
