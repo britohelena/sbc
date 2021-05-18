e853496b592f25b0019d06450f2f38bab5d51639
Tomas-Lopes <56835128+Tomas-Lopes@users.noreply.github.com>
2021-05-18 21:48:22 +0100

    .

--- Parte B/Parte B1/Solucao1.pl
+++ Parte B/Parte B1/Solucao1.pl
@@ -1,13 +1,12 @@
 :-[search].
 
-% -- database:
+/*% -- database:
 %   simple state representation: S, where S is a city
-initial(porto). % initial city
-goal(guimaraes). % destination city
+initial(restaurante). % initial city
+goal(guimaraes). % destination city*/
 
-% --- knowledge base:
-% road(origem,destino, tempo)
-% data collected from: https://pt.distance.to/
+/*--- knowledge base:
+% road(origem,destino, tempo)*/
 road(restaurante,cliente1,5).
 road(restaurante,cliente4,7).
 road(cliente1,cliente4,5).
@@ -19,30 +18,30 @@ road(cliente2,cliente3,3).
 road(cliente5,cliente3,5).
 road(cliente3,cliente4,4).
 
-% travel(City1,City2,distance):
-travel(X,Y,KM):-(road(X,Y,KM);road(Y,X,KM)). % true if road or symmetrical
+/*travel(City1,City2,distance):*/
+travel(X,Y,KM):-(road(X,Y,KM);road(Y,X,KM)). /* true if road or symmetrical*/
 
-% state transition rule s/2: s(City1,City2)
-s(N1,N2):- travel(N1,N2,_). % link s(O,D,Dist) with s(O,D)
+/* state transition rule s/2: s(City1,City2)*/
+s(N1,N2):- travel(N1,N2,_). //link s(O,D,Dist) with s(O,D)
 
-% evaluation function: (sum of distances for all pairs)
+/* evaluation function: (sum of distances for all pairs)
 eval([_],0).
 eval([City1,City2|R],DS):- 
 	travel(City1,City2,D),
 	eval([City2|R],DR),
 	DS is D+DR.
 
-% execute and show a search method result:
+/* execute and show a search method result:*/
 run(Method):- search(Method,Par,S),
 	      write('method:'),write(Method),writepar(Par),nl,
               write('solution:'),write(S),nl,
 	      length(S,N),N1 is N-1,write('solution steps:'),write(N1),nl,
               eval(S,D),write('distance:'),write(D).
-% write parameter (if any):
+/* write parameter (if any):*/
 writepar(X):- integer(X),write(' par:'),write(X). % write X
 writepar(_). % do not write X
 
-% execute 3 example searches:
+/* execute 3 example searches:*/
 q1:- run(depthfirst).
 q2:- run(iterativedeepening).
 q3:- run(breadthfirst).
