5eb39a700012c6971b70b288c9f4fd00a98daa78
Bruno893 <81867991+Bruno893@users.noreply.github.com>
2021-05-24 19:34:03 +0100

    Update interface.pl

--- Parte B/Parte B1/interface.pl
+++ Parte B/Parte B1/interface.pl
@@ -1,5 +1,5 @@
 :-dynamic(fact/1),
-[forward, basedados, proof, baseconhecimento].
+[depthfirst, Solucao1, Solucao2, breadthfirst].
 
 menu:- nl,nl , 	write('********************************************************************************************************'), nl,
 				write('           Seja Bem vindo! Antes de iniciar, qual o seu nome?'),nl,
@@ -8,7 +8,7 @@ menu:- nl,nl , 	write('*********************************************************
 				write('  Prazer em estar a ajuda-lo/a Sr./a '), write(X) ,nl,nl,nl,
 				write('********************************************************************************************************'), nl,
 				write('**                                                                                                    **'), nl,			 
-				write('**                          Descubra qual/quais o/s melhor/es prato/s para si!                        **'), nl,
+				write('**                          Descubra qual/quais a/s melhor/es rota/s para si!                        **'), nl,
 				write('**                      Faremos algumas perguntas para o/a ajudar na sua escolha!                     **'), nl, 
 				write('**                                                                                                    **'), nl,			 
 				write('********************************************************************************************************'), nl,
@@ -24,28 +24,31 @@ avaliarEscolha(2):- write('Foi um prazer ajuda-lo!'), halt.
 avaliarEscolha(other):- write('Introduza uma opcao valida, por favor comece de novo'), menu.
 
 questao1:-	write('********************************************************************************************************'), nl,
-			write('**  Que tipo de dieta leva?'), nl,
+			write('**  Qual o máximo de encomendas que pode levar?'), nl,
 			write('**'),nl,
-			write('**  1 - Carnivora'), nl,
-			write('**  2 - Vegetariana'), nl,
-            write('**  3 - Vegan'), nl,nl,
+			write('**  1 - 1Encomenda'), nl,
+			write('**  2 - 2Encomendas'), nl, nl,
 			read(A4),
 			(
-			(A4 == 1), assert(fact(carnivora)), questao2;
-            (A4 == 2), assert(fact(vegetariana)), questao2;
-            (A4 == 3), assert(fact(vegan)), questao2).
+			(A4 == 1), assert(fact(1Encomenda)), questao2;
+            (A4 == 2), assert(fact(2Encomendas)), questao2.
+            
 
 questao2:-	write('********************************************************************************************************'), nl,
-			write('**  Que tipo de prato vai tomar?'), nl,
+			write('**  Qual o seu destino?'), nl,
 			write('**'),nl,
-			write('**  1 - entrada'), nl,
-			write('**  2 - principal'), nl, 
-			write('**  3 - sobremesa'),nl,
+			write('**  1 - Cliente1'), nl,
+			write('**  2 - Cliente2'), nl, 
+			write('**  3 - Cliente3'),nl,
+			write('**  4 - Cliente4'),nl,
+			write('**  5 - Cliente5'),nl,
 			read(A5),
 			(
-			(A5 == 1), assert(fact(entrada)), questao3;
-            (A5 == 2), assert(fact(principal)), questao3;
-            (A5 == 3), assert(fact(sobremesa)), questao3).
+			(A5 == 1), assert(fact(Cliente1)), resultado;
+            (A5 == 2), assert(fact(Cliente2)), resultado;
+	    (A5 == 3), assert(fact(Cliente3)), resultado;
+	    (A5 == 4), assert(fact(Cliente4)), resultado;
+            (A5 == 5), assert(fact(Cliente5)), resultado).
 			
 questao3:- 	write('********************************************************************************************************'), nl,
 			write('**  Tem alguma alergia? Das seguintes, introduza a correspondente:'), nl, 
@@ -70,6 +73,6 @@ resultado :- 	write('***********************************************************
 resultadowrite(P):-	fact(A6),nl,
 					write('     O seu perfil e o'),nl,
 					write('     *** '),write(P),write(' ***'),nl,nl,
-					write('     Pratos aconselhados: '),perfil(P),nl,nl,
+					write('     Rotas possíveis: '),perfil(P),nl,nl,
 					write('********************************************************************************************************'),
 					retractall(fact(_)).
