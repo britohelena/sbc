dad9bca6b8c52b75debe2e21bb927dc1d1020b46
Tomas-Lopes <56835128+Tomas-Lopes@users.noreply.github.com>
2021-05-25 02:04:10 +0100

    .

--- Parte B/Parte B1/Solucao1.pl
+++ Parte B/Parte B1/Solucao1.pl
@@ -1,12 +1,13 @@
-:-[search], dynamic (goal/1).
-
+:-[search].
+:-dynamic (goal/1).
 
 % -- database:
-initial(restaurante). /*initial city*/
-goal(cliente4). /*destination city*/
+%   simple state representation: S, where S is a city
+initial(restaurante). 
 
-%--- knowledge base:
-% road(origem,destino, tempo)
+% --- knowledge base:
+% road(Origin,Destination,Distance-in-km)
+% data collected from: https://pt.distance.to/
 road(restaurante,cliente1,5).
 road(restaurante,cliente4,7).
 road(cliente1,cliente4,5).
@@ -18,22 +19,19 @@ road(cliente2,cliente3,3).
 road(cliente5,cliente3,5).
 road(cliente3,cliente4,4).
 
-% lucro(cliente, lucro)
 lucro(cliente1, 5).
 lucro(cliente2, 6).
 lucro(cliente3, 7).
 lucro(cliente4, 16).
 lucro(cliente5, 4).
 
-% LucroCaminho
-lucroCaminho([],0).
-lucroCaminho([X|R],Soma) :- lucro(X,TotalCliente), lucroCaminho(R,TotalCaminho), Soma is TotalCliente + TotalCaminho.
+decidirdestino(Destino) :- assert(goal(Destino)).
 
-% travel(partida,chegada,distancia):
-travel(X,Y,KM):-(road(X,Y,KM);road(Y,X,KM)). /* true if road or symmetrical*/
+% travel(City1,City2,distance):
+travel(X,Y,KM):-(road(X,Y,KM);road(Y,X,KM)). % true if road or symmetrical
 
-% state transition rule s/2: s(clientex,clientey)
-s(N1,N2):- travel(N1,N2,_). %link s(O,D,Dist) with s(O,D)
+% state transition rule s/2: s(City1,City2)
+s(N1,N2):- travel(N1,N2,_). % link s(O,D,Dist) with s(O,D)
 
 % evaluation function: (sum of distances for all pairs)
 eval([_],0).
@@ -47,12 +45,13 @@ run(Method):- search(Method,Par,S),
 	      write('method:'),write(Method),writepar(Par),nl,
               write('solution:'),write(S),nl,
 	      length(S,N),N1 is N-1,write('solution steps:'),write(N1),nl,
-              eval(S,D),write('distance:'),write(D).
+              eval(S,D),write('Distancia:'),write(D),nl,goal(A),lucro(A,L),
+			  write("Lucro: "), write(L).
 % write parameter (if any):
 writepar(X):- integer(X),write(' par:'),write(X). % write X
 writepar(_). % do not write X
 
-%execute 3 example searches:
+% execute 3 example searches:
 q1:- run(depthfirst).
 q2:- run(iterativedeepening).
 q3:- run(breadthfirst).
